d50a6dae050e4ff9682914ca8b91167a4461c27a
fedorevgr <lpacwms3000@gmail.com>
2025-05-28 13:04:09 +0300

    Test mesh

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
 
     use std::fs::OpenOptions;
     let mut file = OpenOptions::new().read(true).open("mesh.stl").unwrap();
+    
     let stl = stl_io::read_stl(&mut file).unwrap();
     println!("{:#?}", stl);
 }
