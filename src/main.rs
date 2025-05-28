6ddb36ccf4a49f80cc1ccaf7bb81960c1853e6d9
fedorevgr <lpacwms3000@gmail.com>
2025-05-28 12:53:08 +0300

    etc

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,5 @@ fn main() {
     use std::fs::OpenOptions;
     let mut file = OpenOptions::new().read(true).open("mesh.stl").unwrap();
     let stl = stl_io::read_stl(&mut file).unwrap();
+    println!("{:#?}", stl);
 }
