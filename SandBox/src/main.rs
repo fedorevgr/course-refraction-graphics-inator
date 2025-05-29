4bce75b307da6a3c35e52a84e2e7296c62c2913d
fedorevgr <lpacwms3000@gmail.com>
2025-05-29 22:00:45 +0300

    Matrix4x4

--- SandBox/src/main.rs
+++ SandBox/src/main.rs
@@ -1,6 +1,7 @@
 use std::f64;
 use nalgebra as na;
-use nalgebra::Vector3;
+use nalgebra::Vector4;
+use nalgebra::Matrix4;
 
 
 const FOV_X: f64 = f64::consts::FRAC_PI_3 * 2.0;
@@ -12,10 +13,10 @@ const FOV_Y: f64 = FOV_X / W as f64 * H as f64;
 const LIM_X: f64 = -FOV_X / 2.0;
 const LIM_Y: f64 = -FOV_Y / 2.0;
 
-const VECTOR: Vector3<f64> = Vector3::new(1.0, 0., 0.);
+const VECTOR: Vector4<f64> = Vector4::new(1.0, 0., 0., 0.);
 
 
-fn proj(i: usize, j: usize) -> Option<Vector3<f64>> {
+fn proj(i: usize, j: usize) -> Option<Vector4<f64>> {
     if (i > W) || (j > H) {
         return None;
     }
@@ -23,8 +24,8 @@ fn proj(i: usize, j: usize) -> Option<Vector3<f64>> {
     let delta_x = FOV_X / (W-1) as f64;
     let delta_y = FOV_Y / (H-1) as f64;
     
-    let matrix_yaw = na::Rotation3::from_euler_angles(0., 0., LIM_X + delta_x * i as f64);
-    let matrix_pitch = na::Rotation3::from_euler_angles(0., LIM_Y + delta_y * j as f64, 0.);
+    let matrix_yaw = Matrix4::from_euler_angles(0., 0., LIM_X + delta_x * i as f64);
+    let matrix_pitch = Matrix4::from_euler_angles(0., LIM_Y + delta_y * j as f64, 0.);
     
     Some(matrix_pitch * (matrix_yaw * VECTOR))
 }
