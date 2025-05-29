3ebd291f88cd76481e6de2731bca1558f0bc6ceb
fedorevgr <lpacwms3000@gmail.com>
2025-05-29 20:40:30 +0300

    nice rotatiopn

--- SandBox/src/main.rs
+++ SandBox/src/main.rs
@@ -1,12 +1,36 @@
-use std;
-use std::ops::Mul;
+use std::f64;
 use nalgebra as na;
+use nalgebra::Vector3;
+
+
+const FOV_X: f64 = f64::consts::FRAC_PI_3 * 2.0;
+const W: usize = 40;
+const H: usize = 30;
+
+const FOV_Y: f64 = FOV_X / W as f64 * H as f64;
+
+const LIM_X: f64 = -FOV_X / 2.0;
+const LIM_Y: f64 = -FOV_Y / 2.0;
+
+const VECTOR: Vector3<f64> = Vector3::new(1.0, 0., 0.);
+
+
+fn proj(i: usize, j: usize) -> Vector3<f64> {
+    if (i > W) || (j > H) {
+        panic!("Out of bound at index {}", i);
+    }
+    
+    let delta_x = FOV_X / (W-1) as f64;
+    let delta_y = FOV_Y / (H-1) as f64;
+    
+    let matrix_yaw = na::Rotation3::from_euler_angles(0., 0., LIM_X + delta_x * i as f64);
+    let matrix_pitch = na::Rotation3::from_euler_angles(0., LIM_Y + delta_y * j as f64, 0.);
+    
+    matrix_pitch * (matrix_yaw * VECTOR)
+}
 
 fn main() {
-    let vector = na::Vector4::new(0., 1., 0., 0.);
-    let matrix = na::Matrix4::new_rotation(na::Vector3::new(std::f64::consts::PI / 4.0, 0., 0.));
-    println!("{:?}", matrix);
-    println!("{:#?}", matrix * vector);
-
-    println!("{:#?}", na::Matrix3::new_rotation(1.57));
+    println!("{:.3?}", proj(0, 0));
+    println!("{:.3?}", proj(0, H-1));
+    println!("{:.3?}", proj(W-1, H-1));
 }
