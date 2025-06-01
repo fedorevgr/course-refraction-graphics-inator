81f3a2764f1de5916476f3a1c1f6a8aaaa9e9771
fedorevgr <lpacwms3000@gmail.com>
2025-06-01 15:27:06 +0300

    Material

--- src/renderer/model.rs
+++ src/renderer/model.rs
@@ -1,5 +1,6 @@
+use image::Rgb;
 use crate::renderer::ray::Vector;
-use super::ray;
+use super::ray::Ray;
 
 
 #[derive(Debug)]
@@ -8,15 +9,44 @@ pub struct Triangle {
     pub idx: [usize; 3],
 }
 
+// #[derive(Debug)]
+// pub struct Model {
+//     points: Vec<Vector>,
+//     triangles: Vec<Triangle>,
+// }
+// 
+// impl Model {
+//     pub fn new(points: Vec<Vector>, triangles: Vec<Triangle>) -> Model {
+//         Model { points, triangles }
+//     }
+//     
+// }
+
+#[derive(Debug)]
+pub struct Material {
+    pub color: Rgb<u8>,
+    pub emissive: Rgb<u8>,
+    pub roughness: f64
+}
+
+#[derive(Debug)]
+pub struct Hit {
+    hit: bool,
+    ray: Vec<Ray>,
+}
+
+
 #[derive(Debug)]
 pub struct Model {
-    points: Vec<Vector>,
-    triangles: Vec<Triangle>,
+    center: Vector,
+    radius: f64,
+    material: Material
 }
 
 impl Model {
-    pub fn new(points: Vec<Vector>, triangles: Vec<Triangle>) -> Model {
-        Model { points, triangles }
+    pub fn new(center: Vector, radius: f64, material: Material) -> Model {
+        Model{center, radius, material}
     }
+    
     
 }
