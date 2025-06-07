6aecfb40d80f6e454bccb6947aea9ee4c9ee50cf
fedorevgr <lpacwms3000@gmail.com>
2025-06-07 14:49:26 +0300

    ray distribution

--- SandBox/src/main.rs
+++ SandBox/src/main.rs
@@ -1,46 +1,37 @@
-use egui;
-use eframe;
+use std::fs::File;
+use std::io::Write;
+use rand::{rng};
+use rand_distr::{Distribution, Normal};
 
-use nalgebra::{Vector3, Vector4, Matrix4};
-use rand::distributions::Standard;
+use nalgebra::{Vector4};
 
-#[allow(dead_code)]
-fn gui() -> eframe::Result {
-    println!("Starting GUI");
 
-    let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
-        ..Default::default()
-    };
+const N: u32 = 10;
+const DIFF: f64 = 0.2;
 
-    // Our application state:
-    let mut name = "Arthur".to_owned();
-    let mut age = 42;
 
-    eframe::run_simple_native("My egui App", options, move |ctx, _frame| {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
-            if ui.button("Increment").clicked() {
-                age += 1;
-            }
-            ui.label(format!("Hello '{name}', age {age}"));
-        });
-    })
+fn gen_diffusion(v: &Vector4<f64>) -> Vector4<f64> {
+    let mut rng = rng();
+    let normal = Normal::new(0.0, DIFF).unwrap();
+    let sin = normal.sample(&mut rng).sin();
+    let cos = (1. - sin * sin).sqrt();
+
+    Vector4::new(v.x * cos - v.y * sin, v.x * sin + v.y * cos, v.z, v.w)
 }
 
 
 fn main() {
-    let N = 10;
+    let normal = Vector4::new(0., 1., 0., 0.).normalize();
+    let vector = Vector4::new(1., -1., 0., 0.).normalize();
+    let reflected_ideal: Vector4<f64> =  vector - 2. * (vector.dot(&normal)) * normal;
 
-    let vec = Vector4::new(1.0, -1.0, 1.0, 0.);
-    let normal = Vector4::new(0.0, 1.0, 0.0, 0.);
+    let reflected: Vec<Vector4<f64>> = (0..N)
+        .map(|_| gen_diffusion(&reflected_ideal))
+        .collect();
 
-    let reflected = vec - 2.0 * vec.dot(&normal) * normal;
-    println!("{:.3}, {:.3},  k = {:.3}, {:.3}", vec, normal, 2.0 * normal.dot(&vec), reflected);
+    let mut f = File::create("data.txt").unwrap();
+    f.write_fmt(format_args!("{} {}\n", reflected_ideal.x, reflected_ideal.y)).unwrap();
+    for ray in reflected {
+        f.write_fmt(format_args!("{} {}\n", ray.x, ray.y)).unwrap();
+    }
 }
