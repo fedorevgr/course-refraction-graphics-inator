029d9981d1033d2622481529071aa8215ca94637
fedorevgr <lpacwms3000@gmail.com>
2025-08-02 18:53:02 +0300

    DONE need wraping mul and saturating add - DONE

--- SandBox/src/main.rs
+++ SandBox/src/main.rs
@@ -1,7 +1,33 @@
 use std::num::Saturating;
 
+#[derive(Debug, Copy, Clone)]
+struct ColorVal<T>(pub T);
+
+impl std::ops::Add for ColorVal<u8> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0.saturating_add(rhs.0))
+    }
+}
+
+impl std::ops::Mul for ColorVal<u8> {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self(self.0.wrapping_mul(rhs.0))
+    }
+}
+
+impl From<u8> for ColorVal<u8> {
+    fn from(v: u8) -> ColorVal<u8> {
+        ColorVal(v)
+    }
+}
+
 fn main() {
-    let a: Saturating<u8> = Saturating(80);
-    let b: Saturating<u8> = Saturating(200);
-    println!("{}", a + b);
+    let a = ColorVal(80u8);
+    let b = ColorVal(200u8);
+    println!("{}", (a + b).0);
+    println!("{}", (a * b).0);
 }
