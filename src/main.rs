61c57cce04ef2e8d861c93556db7954a1ce4b079
fedorevgr <lpacwms3000@gmail.com>
2025-08-03 23:15:12 +0300

    Image Managers multithread and not

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-use crate::renderer::objects::camera::{Camera, Dimensions, PerspectiveCamera};
+use crate::renderer::objects::camera::{Dimensions, PerspectiveCamera};
 use crate::renderer::objects::material::{Material, Rgb};
 use crate::renderer::objects::model::TriangleModel;
 use crate::renderer::objects::ray::Vector;
 use crate::renderer::scene::Scene;
-use crate::renderer::{Renderer, SimpleRenderer};
-use image::RgbImage;
-use std::thread;
-use std::thread::JoinHandle;
+use crate::renderer::SimpleRenderer;
+use crate::image_manager::Manager;
 
 mod renderer;
 mod tests;
+mod image_manager;
 
-type Color = image::Rgb<u8>;
+
 
 fn main() {
     let start = std::time::SystemTime::now();
@@ -35,115 +34,13 @@ fn main() {
         .unwrap(),
     ]));
 
-    generate_image(&camera, &renderer, 10)
+    let i_man = image_manager::MultiThread { thread_count: 5};
+    i_man.create(&camera, &renderer)
         .save("artifacts/Cup.png")
         .unwrap();
     println!("Elapsed time: {:?}", start.elapsed().unwrap());
 }
 
-struct Block<C: Camera, R: Renderer> {
-    size: usize,
-    x: usize,
-    y: usize,
-    dimensions: Dimensions,
-    camera: C,
-    renderer: R,
-}
 
-impl<C: Camera + Send + 'static, R: Renderer + Send + 'static> Block<C, R> {
-    pub fn new(
-        size: usize,
-        x: usize,
-        y: usize,
-        dimensions: Dimensions,
-        camera: C,
-        renderer: R,
-    ) -> Self {
-        Self {
-            size,
-            x,
-            y,
-            dimensions,
-            camera,
-            renderer,
-        }
-    }
 
-    pub fn run(self) -> JoinHandle<Vec<Color>> {
-        thread::spawn(move || {
-            let mut x = self.x;
-            let mut y = self.y;
 
-            let mut buffer: Vec<Color> = vec![Color::from([0; 3]); self.size];
-
-            for i in 0..self.size {
-                let ray = self.camera.gen_ray(x, y);
-                let col = self.renderer.cast(&ray);
-
-                buffer[i] = Color::from([col[0], col[1], col[2]]);
-
-                x += 1;
-                if x == self.dimensions.width {
-                    x = 0;
-                    y += 1;
-                }
-            }
-            buffer
-        })
-    }
-}
-
-fn generate_image<C: Camera + Clone + Send + 'static, R: Renderer + Clone + Send + 'static>(
-    camera: &C,
-    renderer: &R,
-    thread_count: usize,
-) -> RgbImage {
-    let dims = camera.get_dimensions();
-    let total = dims.width * dims.height;
-
-    let block_size = total / thread_count;
-    let leftover = total % thread_count;
-
-    let mut threads: Vec<_> = (0..thread_count)
-        .map(|i| {
-            Block::new(
-                block_size,
-                block_size * i % dims.width,
-                block_size * i / dims.width,
-                dims.clone(),
-                camera.clone(),
-                renderer.clone(),
-            )
-            .run()
-        })
-        .collect();
-
-    if leftover > 0 {
-        threads.push(
-            Block::new(
-                leftover,
-                block_size * thread_count % dims.width,
-                block_size * thread_count / dims.width,
-                dims.clone(),
-                camera.clone(),
-                renderer.clone(),
-            )
-            .run(),
-        );
-    }
-
-    let image_segments: Vec<Vec<Color>> =
-        threads.into_iter().map(|h| h.join().unwrap()).collect();
-
-    let _image: Vec<&Color> = image_segments
-        .iter()
-        .flat_map(|segment| segment.as_slice())
-        .collect();
-
-    RgbImage::from_raw(
-        dims.width as u32,
-        dims.height as u32,
-        _image.iter().flat_map(|pix| pix.0).collect(),
-    )
-    .unwrap()
-}
