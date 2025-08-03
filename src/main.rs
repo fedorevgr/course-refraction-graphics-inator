de4836eef8fbdddc9d7e7b9e309d0b3bd16f2dad
fedorevgr <lpacwms3000@gmail.com>
2025-08-03 22:28:29 +0300

    threaded

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use image::{RgbImage};
+use image::RgbImage;
+use std::thread;
 
 use crate::renderer::objects::camera::{Camera, Dimensions, PerspectiveCamera};
 use crate::renderer::objects::material::{Material, Rgb};
@@ -13,30 +14,35 @@ mod tests;
 type Color = image::Rgb<u8>;
 
 fn main() {
+    let start = std::time::SystemTime::now();
     let camera = PerspectiveCamera::new(
         Vector::new(7.2, -4.2, 6.4, 0.),
         Vector::new(0., 0., 0., 0.),
-        Dimensions {width: 400, height: 300},
+        Dimensions {
+            width: 400,
+            height: 300,
+        },
         std::f64::consts::FRAC_PI_6,
     );
 
     let renderer = SimpleRenderer::new(Scene::new(vec![
-        TriangleModel::from_stl(
-            "Cup.stl",
-            {
-                let mut m = Material::metallic();
-                m.roughness = Rgb::from([20, 20, 20]);
-                m.k = 3.;
-                m
-            }
-        ).unwrap()
+        TriangleModel::from_stl("Cup.stl", {
+            let mut m = Material::metallic();
+            m.roughness = Rgb::from([20, 20, 20]);
+            m.k = 3.;
+            m
+        })
+        .unwrap(),
     ]));
 
-    generate_image(&camera, &renderer, 2).save("artifacts/Cup.png").unwrap();
+    generate_image(&camera, &renderer, 10)
+        .save("artifacts/Cup.png")
+        .unwrap();
+    println!("Elapsed time: {:?}", start.elapsed().unwrap());
 }
 
 struct Block<C: Camera, R: Renderer> {
-    buffer: Vec<Color>,
+    size: usize,
     x: usize,
     y: usize,
     dimensions: Dimensions,
@@ -45,26 +51,35 @@ struct Block<C: Camera, R: Renderer> {
 }
 
 impl<C: Camera, R: Renderer> Block<C, R> {
-    pub fn new(size: usize, x: usize, y: usize, dimensions: Dimensions, camera: C, renderer: R) -> Self {
+    pub fn new(
+        size: usize,
+        x: usize,
+        y: usize,
+        dimensions: Dimensions,
+        camera: C,
+        renderer: R,
+    ) -> Self {
         Self {
-            buffer: vec![Color::from([0; 3]); size],
+            size,
             x,
             y,
             dimensions,
             camera,
-            renderer
+            renderer,
         }
     }
 
-    pub fn run(&mut self) {
+    pub fn run(self) -> Vec<Color> {
         let mut x = self.x;
         let mut y = self.y;
-        for i in 0..self.buffer.len() {
 
+        let mut buffer: Vec<Color> = vec![Color::from([0; 3]); self.size];
+
+        for i in 0..self.size {
             let ray = self.camera.gen_ray(x, y);
             let col = self.renderer.cast(&ray);
 
-            self.buffer[i] = Color::from([col[0], col[1], col[2]]);
+            buffer[i] = Color::from([col[0], col[1], col[2]]);
 
             x += 1;
             if x == self.dimensions.width {
@@ -72,29 +87,56 @@ impl<C: Camera, R: Renderer> Block<C, R> {
                 y += 1;
             }
         }
+        buffer
     }
 }
 
-fn generate_image<C: Camera + Clone, R: Renderer + Clone>(
-    camera: &C, renderer: &R, threads: usize
+fn generate_image<C: Camera + Clone + Send + 'static, R: Renderer + Clone + Send + 'static>(
+    camera: &C,
+    renderer: &R,
+    thread_count: usize,
 ) -> RgbImage {
     let dims = camera.get_dimensions();
     let total = dims.width * dims.height;
 
-    let block_size = total / threads;
-    let leftover = total % threads;
+    let block_size = total / thread_count;
+    let leftover = total % thread_count;
 
-    let mut block_placements: Vec<_> = (0..threads).map(|i|
-        Block::new(block_size, block_size * i % dims.width, block_size * i / dims.width, dims.clone(), camera.clone(), renderer.clone())
-    ).collect();
+    let threads: Vec<_> = (0..thread_count)
+        .map(|i| {
+            let block = Block::new(
+                block_size,
+                block_size * i % dims.width,
+                block_size * i / dims.width,
+                dims.clone(),
+                camera.clone(),
+                renderer.clone(),
+            );
+            thread::spawn(move || block.run())
+        })
+        .collect();
+
+    let mut image_segments: Vec<Vec<Color>> = threads.into_iter().map(|h| h.join().unwrap()).collect();
+
     if leftover > 0 {
-        block_placements.push(Block::new(leftover, block_size * threads % dims.width, block_size * threads / dims.width, dims.clone(), camera.clone(), renderer.clone()));
+        image_segments.push(
+            Block::new(
+                leftover,
+                block_size * thread_count % dims.width,
+                block_size * thread_count / dims.width,
+                dims.clone(),
+                camera.clone(),
+                renderer.clone(),
+            )
+            .run(),
+        );
     }
 
-    block_placements.iter_mut().for_each(Block::run);
 
-    let _image: Vec<&Color> = block_placements.iter().flat_map(|block| block.buffer.as_slice()).collect();
+    let _image: Vec<&Color> = image_segments
+        .iter()
+        .flat_map(|segment| segment.as_slice())
+        .collect();
 
     RgbImage::from_raw(dims.width as u32, dims.height as u32, _image.iter().flat_map(|pix| pix.0).collect()).unwrap()
 }
-
