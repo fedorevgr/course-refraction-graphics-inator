88347564a0dd348c6d6661f02d6e89b04ebbd165
fedorevgr <lpacwms3000@gmail.com>
2025-08-13 16:38:10 +0300

    wuaaaaah

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,71 @@
+mod image_manager;
 mod renderer;
 mod tests;
-mod image_manager;
 
-use image::{Rgb, RgbImage};
-use rand::{Rng, SeedableRng};
-use crate::renderer::objects::camera::Dimensions;
-use crate::renderer::objects::camera::perspective::PerspectiveCamera;
-use crate::renderer::objects::material::MaterialBuilder;
-use crate::renderer::objects::ray::Vector;
-use crate::renderer::scene::Scene;
-use crate::renderer::implementations::sampling::{Sampling, Black};
-use crate::image_manager::Manager;
-use crate::renderer::objects::model::sphere::SphereModel;
+use clap::{Parser};
+use eframe::{NativeOptions, egui};
 
-fn main() {
-    let camera = PerspectiveCamera::new(
-        Vector::new(0., -10., 0., 0.),
-        Vector::new(0., 0., 0., 0.),
-        Dimensions {
-            width: 60,
-            height: 40,
-        },
-        std::f64::consts::FRAC_PI_6 / 1.,
-    );
+fn main() -> eframe::Result {
+    let config = Arguments::parse();
 
-    let scene = Scene::new(vec![
-        SphereModel::new(
-            Vector::from([0.; 4]),
-            1.,
-            MaterialBuilder::default()
-                .color([20; 3].into())
-                .roughness([0xFF;3].into())
-                .emissivity([0x10; 3].into())
-                .build().unwrap()
-        ),
-        SphereModel::new(
-            Vector::from([-2., 0., 0., 0.]),
-            1.,
-            MaterialBuilder::default()
-                .color([0; 3].into())
-                .roughness([0; 3].into())
-                .emissivity([0xFF; 3].into())
-                .build().unwrap()
-        ),
-    ]
-    );
+    let native_options = NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([config.camera_width as f32, config.camera_height as f32])
+            .with_taskbar(true)
+            .with_fullscreen(true)
+        ,
 
-    let renderer = Sampling::new(
-        scene,
-        Black{},
-        5,
-        rand_chacha::ChaCha8Rng::seed_from_u64(0),
-    );
-
-    // let manager = image_manager::implementations::rayon::Library::new(64 * 64);
-    let manager = image_manager::implementations::one_thread::OneThreaded{};
-    manager.create(&camera, &renderer).save("artifacts/Cube.png").unwrap();
-
-    let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(1);
-    RgbImage::from_fn(1920, 1080, |_, _| Rgb([rng.random(); 3])).save("artifacts/Noise.png").unwrap();
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Refraction App",
+        native_options,
+        Box::new(|cc|
+            Ok(Box::new(RefractionApp::new(cc, [config.camera_width as f32, config.camera_height as f32])))
+        ))
 }
 
+#[derive(Default)]
+struct RefractionApp {
+    dims: [f32; 2],
+    value: bool
+}
 
+impl RefractionApp {
+    fn new(cc: &eframe::CreationContext<'_>, dims: [f32; 2]) -> Self {
+        Self {
+            dims,
+            value: false
+        }
+    }
+}
 
+impl eframe::App for RefractionApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                egui::Frame::canvas(ui.style()).show(ui, |ui| {
+                    let (rect, response) =
+                        ui.allocate_exact_size([400., 400.].into(), egui::Sense::drag());
+                });
+            });
+            ui.toggle_value(&mut self.value, "Open").clicked();
 
+            if self.value {
+                egui::SidePanel::left("my_left_panel").show(ctx, |ui| {
+                    ui.label("Hello World!");
+                    ui.toggle_value(&mut self.value, "Close");
+                });
+            }
+        });
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Arguments {
+    #[arg(long = "width", default_value_t = 640)]
+    camera_width: usize,
+    #[arg(long = "height", default_value_t = 480)]
+    camera_height: usize,
+}
